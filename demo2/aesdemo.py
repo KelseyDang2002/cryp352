305dcf58e9f3be6ade7ce7ad6504b1e5a9e3cbeb
Kenny111111 <kennydang424@gmail.com>
2024-02-05 21:50:57 -0800

    update

--- demo2/aesdemo.py
+++ demo2/aesdemo.py
@@ -6,17 +6,20 @@ from Cryptodome.Cipher import AES
 
 ######### BASIC ENCRYPTION ###########
 
-# The key (must be 16 bytes)
-key = b'Sixteen byte key'
+# The key (must be 16 bytes = 128 bit)
+key = b'Sixteen byte key' # b = raw byte
 
 # Set up the AES encryption class
-encCipher = AES.new(key, AES.MODE_ECB)
+encCipher = AES.new(key, AES.MODE_ECB) # new used to create instance
 
 # AES requires plain/cipher text blocks to be 16 bytes
-cipherText = encCipher.encrypt(b'hello12345678s0d1111111111111111')
+cipherText = encCipher.encrypt(b'hello12345678s0d1111111111111111') # 2 blocks worth
 
 print("Cipher text: ", cipherText)
 
+cipherFile = open("encrypted.enc", "wb")
+cipherFile.write(cipherText)
+cipherFile.close()
 
 ########### BASIC DECRYPTION ##############
 
@@ -27,3 +30,7 @@ decCipher = AES.new(key, AES.MODE_ECB)
 plainText = decCipher.decrypt(cipherText)
 
 print("Decrypted text: ", plainText)
+
+plainFile = open("plain.dec", "wb")
+plainFile.write(plainText)
+plainFile.close()
