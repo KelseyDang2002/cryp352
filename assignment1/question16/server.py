1b1da538ca394025d8ebd83612bc50b104109085
KelseyDang2002 <kelseydang813@gmail.com>
2024-02-17 11:23:43 -0800

    q16 done

--- assignment1/question16/server.py
+++ assignment1/question16/server.py
@@ -1,25 +1,51 @@
 import socket
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
+
 ####### A SIMPLE ILLUSTRATION OF THE TCP SERVER #######
-# The port number on which to listen for incoming
-# connections.
+
+# The port number on which to listen for incoming connections.
 PORT_NUMBER = 1235
+KEY = input("Enter 16-byte key: ").encode('utf-8')
+
 # Create a socket
 serverSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 # Associate the socket with the port
-serverSock.bind(('', PORT_NUMBER))
-# Start listening for incoming connections (we can have
-# at most 100 connections waiting to be accepted before
-# the server starts rejecting new connections)
+serverSock.bind(('127.0.0.1', PORT_NUMBER))
+
+# Start listening for iuncoming conncetions (we can have at 
+# most 100 concetions waiting to be accepted before the server
+# starts rejecting new connections)
 serverSock.listen(100)
-# Keep accepting connections forever
+
+# Keep accepting conncetions forever
 while True:
-print("Waiting for clients to connect...")
-# Accept a waiting connection
-cliSock, cliInfo = serverSock.accept()
-print("Client connected from: " + str(cliInfo))
-# Receive the data the client has to send.
-# This will receive at most 1024 bytes
-cliMsg = cliSock.recv(1024)
-print("Client sent " + str(cliMsg))
-# Hang up the client's connection
-cliSock.close()
+    print("\nWaiting for clients to connect...")
+
+    # Accept a waiting connection
+    cliSock, cliInfo = serverSock.accept()
+
+    print("Client connected from: " + str(cliInfo))
+
+    # Receive the data the client has to send.
+    # This will receive at most 1024 bytes
+    cliMsg = cliSock.recv(1024)
+    print("\tReceived: " + str(cliMsg))
+
+    # Decrypt the message
+    cipher = AES.new(KEY, AES.MODE_ECB)
+    decryptedMsg = cipher.decrypt(cliMsg)
+    print("\tDecrypted: " + str(decryptedMsg))
+
+    # Unpad msg
+    unpaddedMsg = unpad(decryptedMsg, AES.block_size)
+    print("\tUnpadded: " + str(unpaddedMsg))
+    
+    # Decode msg
+    finalMsg = unpaddedMsg.decode('utf-8')
+
+    print("\tFinal decrypted message: " + str(finalMsg))
+
+    # Hang up the client's connection
+    cliSock.close()
