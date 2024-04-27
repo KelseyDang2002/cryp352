8a1ba9ce1a3759367928026fff93d4490873f374
KelseyDang2002 <kelseydang813@gmail.com>
2024-04-27 10:46:22 -0700

    update

--- assignment3/question7.py
+++ assignment3/question7.py
@@ -1,11 +1,35 @@
 # Assignment 3 Question 7
 
+'''
+Requirements:
+
+1. Prompt the user to enter two options (1) create an account or
+login into the existing account
+
+2. If the user chooses option (1) they are prompted with to select a username and password.
+The program will then check file called db.txt that has the following format:
+
+    each line consists of the:
+    - username
+    - space separator
+    - salted hashed password (computed using the bcrypt library)
+
+associated with the user.
+
+3. If the user has a registered account in db.txt and enters a valid password 
+(that is properly matched against the salted hash), they can attempt to login 
+into their account by choosing option (2) at the intial prompt.
+
+Now if the user enters a wronf password or the account with the specifies username does not exist,
+the login should be denied.
+'''
+
 import bcrypt
 import pwinput
 
 # prompt user with options
 
-selected_option = int(input("Choose (1) to create an account or; (2) to log into exisitng account: "))
+selected_option = int(input("Choose (1) to create an account or; (2) to log into existing account: "))
 
 if selected_option == 1:
     print(selected_option)
