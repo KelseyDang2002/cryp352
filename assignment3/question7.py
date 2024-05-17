b0115b8ec8c72e6964d69ab29b580f3a769092e8
KelseyDang2002 <kelseydang813@gmail.com>
2024-05-16 22:42:37 -0700

    TODO: read hashed password from db.txt

--- assignment3/question7.py
+++ assignment3/question7.py
@@ -20,22 +20,76 @@ associated with the user.
 (that is properly matched against the salted hash), they can attempt to login 
 into their account by choosing option (2) at the intial prompt.
 
-Now if the user enters a wronf password or the account with the specifies username does not exist,
+Now if the user enters a wrong password or the account with the specifies username does not exist,
 the login should be denied.
 '''
 
 import bcrypt
 import pwinput
 
-# prompt user with options
+def create_account():
+    username = input("Pick a username: ")
+    password = pwinput.pwinput(prompt = "Pick a password: ")
+    print("\nYou entered: ", type(username),username, " ", type(password), password)
+    return username, password
 
+def log_in():
+    username = input("Enter existing username: ")
+    password = pwinput.pwinput(prompt = "Enter the password: ")
+    print("\nYou entered: ", type(username),username, " ", type(password), password)
+    return username, password
+
+def hash_password(password):
+    password_in_bytes = bytes(password, 'utf-8')
+    password_in_bytes = password.encode('utf-8')
+    print("Password converted to bytes: ", type(password_in_bytes), password_in_bytes)
+    salt = bcrypt.gensalt()
+    print("\nSalt: ", salt)
+    hashed_password = bcrypt.hashpw(password_in_bytes, salt)
+    print("Hashed password: ", type(hashed_password), hashed_password)
+    return hashed_password
+
+def store_in_db(username, hashed_password):
+    account = username + " " + hashed_password + "\n"
+    print("\nAppend ", account, "to db.txt")
+    print(type(account))
+    file_append = open("db.txt", "a")
+    file_append.write(account)
+    file_append.close
+    print("\nAccount successfully created! Run the program again and try to log in.")
+    # return username, hashed_password
+
+def check_if_in_db(username, password):
+    password_in_bytes = bytes(password, 'utf-8')
+    password_in_bytes = password.encode('utf-8')
+    print("\nUsername: ", username)
+    print("Password: ", password_in_bytes)
+    file_read = open("db.txt", "r")
+
+    for accounts in file_read:
+        account = accounts.strip()
+        print(account)
+
+    file_read.close()
+
+    hashed_password_in_db = "read from file"
+    
+    # if bcrypt.checkpw(password_in_bytes, hashed_password_in_db):
+    #     print("Passwords match!")
+    # else:
+    #     print("Incorrect password or account does not exist.")
+
+# prompt user with options
 selected_option = int(input("Choose (1) to create an account or; (2) to log into existing account: "))
 
 if selected_option == 1:
-    print(selected_option)
+    username, password = create_account()
+    hashed_password = hash_password(password)
+    store_in_db(username, hashed_password)
 
 elif selected_option == 2:
-    print(selected_option)
+    username, password = log_in()
+    check_if_in_db(username, password)
 
 else:
     print("Invalid option. Try again.")
--- demo5/createhashpass.py
+++ demo5/createhashpass.py
@@ -3,23 +3,20 @@
 import bcrypt
 
 # The password to hash
-# password = b'hello'
+password = b'hello'
 
 # Generate the salt value
 salt = bcrypt.gensalt()
 
-# print("The salt is ", salt)
-
-enteredpass = input("Please enter a password: ")
+print("The salt is ", salt)
 
 # Hash the password
-# hashed = bcrypt.hashpw(password, salt)
-hashed = b'$2b$12$TUChp.a11tYo20sF8xlL9e30PeqcuWQOQ5KXbMSmzp/b/Vf3/o8qy'
+hashed = bcrypt.hashpw(password, salt)
 
 print("The hash is: ", hashed)
 
 # Verify if the password matches
-if bcrypt.checkpw(enteredpass.encode(), hashed):
+if bcrypt.checkpw(password, hashed):
     print("Passwords match!")
 else:
     print("Passwords DO NOT match!")
